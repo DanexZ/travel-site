f95bee183eeeaddf4f2451bfe9c566880ec79515
Daniel <daniel.zawadzki4@gmail.com>
2019-08-05 22:25:55 +0200

    Completed header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,13 @@ autoprefixer = require('autoprefixer'), //prefixuje
 cssvars = require('postcss-simple-vars'), //pozwala na używanie zmiennych
 nested = require('postcss-nested'), //pozwala na zagniedżanie
 cssImport = require('postcss-import'), //importowaniemodularnych css i scalanie z plikiem głównym
-mixins = require('postcss-mixins')
-; 
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba'); 
 
 gulp.task('styles', function(){
     
     return gulp.src('./app/assets/css/style.css')
-        .pipe( postcss([cssImport, mixins, cssvars, nested, autoprefixer]) )
+        .pipe( postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]) )
         .on('error', function(errorInfo){
             //wyświetl informacje o błędzie i to w odpowiedni sposób
             console.log( errorInfo.toString() );
